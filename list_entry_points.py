de0a96d0b3c3630a876f31668e4673da32479d13
Dash <16400857+analogdash@users.noreply.github.com>
2018-11-05 03:28:09 +0800

    Update list_entry_points.py

--- list_entry_points.py
+++ list_entry_points.py
@@ -14,9 +14,28 @@ sample_path = r'C:\samples'
 file_list = loadinfo(sample_path)
 
 f = open("demofile.csv", "a")
+f.write("filename")
+f.write(",")
+f.write("ep")
+f.write(",")
+f.write("imagebase")
+f.write(",")
+f.write("no of sections")
+f.write(",")
+f.write("first hex bytes")
+f.write("\n")
+
 for item in file_list:
-    f.write(item["filename"]+","+hex(item["pe"].OPTIONAL_HEADER.AddressOfEntryPoint)+"\n")
+    f.write(item["filename"])
+    f.write(",")
+    f.write(hex(item["pe"].OPTIONAL_HEADER.AddressOfEntryPoint))
+    f.write(",")
+    f.write(hex(item["pe"].OPTIONAL_HEADER.ImageBase))
+    f.write(",")
+    f.write(len(item["pe"].sections))
+    f.write(",")
+    f.write(item["pe"].get_data(item["pe"].OPTIONAL_HEADER.AddressOfEntryPoint, 16).hex())
+    f.write("\n")
 
 f.close()
-
 
